dc91626759e24d3ad4c671764357f2cc1f101dbb
Roberto Mauro Perez Olivera <robertom.perez@outlook.com>
2023-10-10 21:45:23 -0300

    corregido

--- src/home.js
+++ src/home.js
@@ -12,23 +12,32 @@ const characterList = async (page = 1) => {
     loader.style.display = 'grid'
 
     const { results } = await getCharacters(page)
-
+ console.log(results)
     loader.style.display = 'none'
 
     results.forEach(character => {
          const article = document.createElement('article')
-         article.setAttribute('class', "character")
-         article.innerHTML =`  <img src="${character.image}">
+         article.setAttribute('class','character')
+         article.innerHTML =`  
+         <img src="${character.image}">
         <h2>${character.name}</h2>
          <div>
          <p>${character.species}</p>
          <p class='${character.status}'></p>
 
          </div>
-        <a href="/#/${character.id}" >Ver detalle</a>`
+        <a href="/#/${character.id}" >Ver detalle</a>
+        `
+        // esto es lo que producia el error
+        container.appendChild(article)
     });
-
-    container.appendChild(article)
+    
 }
 
 characterList()
+
+window.addEventListener('hashchange',() => {
+    const id = location.hash.slice(1).toLocaleLowerCase().split('/')[1]|| '/'
+    localStorage.setItem('charID', id)
+    window.location.replace('/character.html')
+})
